74d1d45873b9e23ec27b93a36a5760ed33a20b5d
wpforchurch <wpforchurch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-09 17:51:07 +0000

    1.9 release
    git-svn-id: https://plugins.svn.wordpress.org/sermon-manager-for-wordpress/trunk@890227 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -7,7 +7,8 @@
  */
  
 // List all series or speakers in a simple unordered list
-add_shortcode('list-sermons', 'wpfc_list_sermons_shortcode');
+add_shortcode('list_sermons', 'wpfc_list_sermons_shortcode'); //preferred markup
+add_shortcode('list-sermons', 'wpfc_list_sermons_shortcode'); //left for compatibility
 function wpfc_list_sermons_shortcode( $atts = array () ){
 	extract( shortcode_atts( array(
 		'tax' => 'wpfc_sermon_series', // options: wpfc_sermon_series, wpfc_preacher, wpfc_sermon_topics, wpfc_bible_book
@@ -28,7 +29,8 @@ function wpfc_list_sermons_shortcode( $atts = array () ){
 }
  
 // Display all series or speakers in a grid of images
-add_shortcode('sermon-images', 'wpfc_display_images_shortcode');
+add_shortcode('sermon_images', 'wpfc_display_images_shortcode'); //preferrred markup
+add_shortcode('sermon-images', 'wpfc_display_images_shortcode'); //left for compatibility
 function wpfc_display_images_shortcode( $atts = array () ) {
 	extract( shortcode_atts( array(
 		'tax' => 'wpfc_sermon_series', // options: wpfc_sermon_series, wpfc_preacher, wpfc_sermon_topics
@@ -50,6 +52,122 @@ function wpfc_display_images_shortcode( $atts = array () ) {
 	return $list;
 }
 
+/**
+ * Get the latest sermon ID
+ */
+function wpfc_get_latest_sermon( $service_type = 0 ) {
+	$args = array(
+		'post_type' => 'wpfc_sermon',
+		'posts_per_page' => 1,
+		'post_status' => 'publish',
+		'no_found_rows' => true,
+		'update_post_term_cache' => false,
+		'update_post_meta_cache' => false
+		);
+
+	if ( !empty( $service_type ) ) {
+					
+		$tax_args = array(
+			'wpfc_service_type' => $service_type,
+		);
+		$args = array_merge( $args, $tax_args );
+	}
+	$latest_sermon = new WP_Query($args);
+	
+	if ($latest_sermon->have_posts()) : while ($latest_sermon->have_posts()) : $latest_sermon->the_post(); 
+		$latest_id = get_the_ID();
+			return $latest_id;
+		
+	endwhile; wp_reset_postdata(); endif;
+}		
+
+function wpfc_get_latest_series ( $latest_sermon = 0, $service_type = 0 ) {
+	if ( empty( $latest_sermon ) ) {
+		$latest_sermon = wpfc_get_latest_sermon( $service_type );
+	}
+	$latest_series = wp_get_object_terms( $latest_sermon, 'wpfc_sermon_series' );
+	foreach($latest_series as $series_object){
+		return $series_object;
+	}
+}
+
+function wpfc_get_latest_series_image_id ( $latest_series = 0 ) {
+	$associations = sermon_image_plugin_get_associations();
+	$tt_id = absint( $latest_series->term_taxonomy_id );
+
+	$ID = 0;
+	if ( array_key_exists( $tt_id, $associations ) ) {
+			$ID = absint( $associations[$tt_id] );
+	}
+
+	return $ID;
+}
+
+// Display the latest sermon series image (optional - by service type)
+add_shortcode( 'latest_series', 'wpfc_get_latest_series_image' );
+function wpfc_get_latest_series_image ( $atts ) {
+	extract( shortcode_atts( array(
+		'after'     	=> '',
+		'before'		=> '',
+		'image_class'	=> 'latest-series-image',
+		'size'			=> 'large',
+		'show_title'	=> true,
+		'title_wrapper'	=> 'h3', //options p, h1, h2, h3, h4, h5, h6, div
+		'title_class'	=> 'latest-series-title',
+		'service_type'	=> '', //use the service type slug
+		'show_desc'		=> false,
+		'wrapper_class'	=> 'latest-series'
+	), $atts, 'latest_series' ) );
+	
+	$latest_sermon = wpfc_get_latest_sermon( $service_type );
+	$latest_series = wpfc_get_latest_series( $latest_sermon );
+	$series_link = get_term_link( $latest_series, 'wpfc_sermon_series' );
+	$series_image_id = wpfc_get_latest_series_image_id ( $latest_series );
+	
+	if ( empty( $series_image_id ) ) {
+		return;
+	}
+	$image_size = sanitize_key( $size );
+	$image_class = sanitize_text_field( $image_class );
+	$show_title = wpfc_sanitize_bool( $show_title );
+	$title_wrapper = sanitize_text_field( $title_wrapper );
+	$wrapper_options = array( 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'div' );
+        if( ! in_array( $title_wrapper, $wrapper_options ) )
+                $title_wrapper = 'h3';
+	$title_class = sanitize_text_field( $title_class );
+	$show_desc = wpfc_sanitize_bool( $show_desc );
+	
+	$before = sanitize_text_field( $before ); 
+	$after = sanitize_text_field( $after );
+	
+	$link_open = '<a href="' . $series_link . '" title="' . $latest_series->name . '" alt="' . $latest_series->name . '">';
+	$link_close = '</a>';
+	
+	$image = wp_get_attachment_image( $series_image_id, $image_size, false, array( 'class' => $image_class ) ); 
+	
+	$title = $description = '';
+	if( $show_title ) {
+		$title = $latest_series->name;
+		$title = '<' . $title_wrapper . ' class="' . $title_class . '">' . $title . '</' . $title_wrapper . '>';
+	}
+	if( $show_desc ) {
+		$description = '<div class="latest-series-description">' . wpautop( $latest_series->description ) . '</div>'; 
+	}
+	
+	$output = $before . $link_open . $image . $title . $link_close . $description . $after;
+		return $output;
+	
+}
+
+/**
+ * Convert string to boolean
+ * because (bool) "false" == true
+ *
+ */
+function wpfc_sanitize_bool( $value ) {
+        return !empty( $value ) && 'true' == $value ? true : false;
+}
+
 // Create the shortcode
 add_shortcode('sermons', 'wpfc_display_sermons_shortcode');
 function wpfc_display_sermons_shortcode($atts) {
@@ -64,8 +182,8 @@ function wpfc_display_sermons_shortcode($atts) {
 		'tax_term' => false,
 		'image_size' => 'sermon_small',
 		'tax_operator' => 'IN'
-	), $atts ) );
-	// begin - code from : http://wordpress.org/support/topic/wp-pagenavi-with-custom-query-and-paged-variable?replies=2
+	), $atts, 'sermons' ) );
+	// pagination
 		global $paged;
 		if( get_query_var( 'paged' ) )
 			$my_page = get_query_var( 'paged' );
@@ -77,7 +195,7 @@ function wpfc_display_sermons_shortcode($atts) {
 		set_query_var( 'paged', $my_page );
 		$paged = $my_page;
 		}
-	// - end
+	// pagination end
 	$args = array(
 		'post_type' => 'wpfc_sermon',
 		'posts_per_page' => $posts_per_page,
@@ -132,15 +250,23 @@ function wpfc_display_sermons_shortcode($atts) {
 		<?php render_wpfc_sermon_excerpt(); ?>
 	</div>
 	<?php
-	endwhile; //end loop
-	if( function_exists("wp_pagenavi") ) : ?>
-		<div id="sermon-navigation"> 
-			<?php wp_pagenavi( array( 'query' => $listing ) ); ?>
-		</div>
-	<?php
-	endif;
-	wp_reset_query();
-	?>
+	endwhile; //end loop ?>
+		<div style="clear:both;"></div>
+
+			<?php wp_reset_postdata(); ?>
+
+			<div id="sermon-navigation">
+				<?php
+					$big = 999999;
+					echo paginate_links( array(
+						'base'    => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+						'format'  => '?paged=%#%',
+						'current' => max( 1, $args['paged'] ),
+						'total'   => $listing->max_num_pages
+					) );
+				?>
+			</div>
+		<div style="clear:both;"></div>
 	</div>
 	</div>
 	<?php
